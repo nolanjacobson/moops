610864231b647733de29f4f00ea680313e34f3f1
Nolan Jacobson <theprophet0@users.noreply.github.com>
2020-02-18 09:32:24 -0500

    posting webtorrent magnet to blockchain test

--- src/pages/Studio.jsx
+++ src/pages/Studio.jsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react'
+import StudioStyles from '../CSS/StudioPage.css'
+
 import BlankProfileImage from '../images/Blank-profile.png'
 // import { ReactMediaRecorder } from 'react-media-recorder'
 import VideoRecorder from 'react-video-recorder'
 import steem from 'steem'
+let WebTorrent = require('webtorrent')
+let client = new WebTorrent()
 const Studio = props => {
   const [testBool, setTestBool] = useState(false)
 
   const [profileImage, setProfileImage] = useState(BlankProfileImage)
   const [username, setUsername] = useState('')
+  const [video, setVideo] = useState('')
+  const [webTorrentMagnet, setWebTorrentMagnet] = useState('')
   const permlink = Math.random()
     .toString(36)
     .substring(2)
@@ -16,13 +22,50 @@ const Studio = props => {
     console.log(props.client.accessToken)
     setTestBool(true)
     var query = {
-      tag: 'steemit', // This tag is used to filter the results by a specific post tag
+      tag: 'moops', // This tag is used to filter the results by a specific post tag
       limit: 5, // This limit allows us to limit the overall results returned to 5
     }
     steem.api.getDiscussionsByTrending(query, function(err, result) {
       console.log(err, result)
     })
   }, [])
+  const makeNewPost = () => {
+    props.client.comment(
+      '',
+      'moops',
+      username,
+      permlink,
+      'WebTorrent Test',
+      webTorrentMagnet,
+      JSON.stringify({ tags: 'moops' }),
+      (err, res) => {
+        console.log(err, res)
+      }
+    )
+  }
+  useEffect(() => {
+    if (video !== '') {
+      // console.log(video)
+      // let buf = new Buffer(video)
+      // buf.name = 'Some file name'
+      // client.seed(buf, torrent => {
+      //   console.log('Client is seeding ' + torrent.magnetURI)
+      // })
+      client.seed(video, torrent => {
+        console.log('test', torrent.magnetURI)
+        setWebTorrentMagnet(torrent.magnetURI)
+      })
+      client.on('torrent', () => {
+        makeNewPost()
+      })
+    }
+  }, [video])
+
+  // useEffect(() => {
+  //   if (webTorrentMagnet) {
+  //     makeNewPost()
+  //   }
+  // }, [webTorrentMagnet])
   useEffect(() => {
     if (testBool) {
       props.client.me((err, res) => {
@@ -37,14 +80,22 @@ const Studio = props => {
   }, [testBool])
   return (
     <>
-      <div>
-        <img className="profilePicture" src={profileImage} />
-      </div>
-      <div>
+      <span>
+        <img className="studioProfilePicture" src={profileImage} />
+        <i class="fas fa-ellipsis-v" aria-hidden="true"></i>
+      </span>
+      <div className="camera">
         <VideoRecorder
+          // isOnInitially={true}
+          // showReplayControls={true}
+          // replayVideoAutoplayAndLoopOff={true}
+          // timeLimit={7000}
           onRecordingComplete={videoBlob => {
-            console.log('videoBlob', videoBlob)
+            setVideo(videoBlob)
           }}
+          // onStopRecording={videoBlob => {
+          //   setVideo(videoBlob)
+          // }}
         />
       </div>
     </>
